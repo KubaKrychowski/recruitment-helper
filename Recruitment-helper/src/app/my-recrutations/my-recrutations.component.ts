42aa709b727a97e118d6c56cab13949836fe8196
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-08-14 07:24:31 +0200

    added get recrutations list request

--- Recruitment-helper/src/app/my-recrutations/my-recrutations.component.ts
+++ Recruitment-helper/src/app/my-recrutations/my-recrutations.component.ts
@@ -1,15 +1,46 @@
+import { ApiService } from './../services/api.service';
 import { Component, OnInit } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-my-recrutations',
   templateUrl: './my-recrutations.component.html',
-  styleUrls: ['./my-recrutations.component.scss']
+  styleUrls: ['./my-recrutations.component.scss'],
 })
 export class MyRecrutationsComponent implements OnInit {
-
-  constructor() { }
+  recrutations: any = [];
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
+    this.getAllRecrutations();
   }
 
+  private getAllRecrutations(): void {
+    this.apiService
+      .sendGetRequest('AddNewRecrutation')
+      .pipe(
+        catchError(this.errorHandler),
+        tap((result) => {
+          this.recrutations = result;
+        })
+      )
+      .subscribe(() => {
+        return;
+      });
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.log('An error occurred:', error.error);
+    } else {
+      console.log(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
+  }
 }
--- Recruitment-helper/src/app/services/api.service.ts
+++ Recruitment-helper/src/app/services/api.service.ts
@@ -9,7 +9,10 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   sendPostRequest(requestBody: any){
-    console.log(requestBody);
     return this.http.post('https://localhost:44339/api/AddNewRecrutation', requestBody);
+  }
+
+  sendGetRequest(url: string){
+    return this.http.get(`https://localhost:44339/api/${url}`);
   }
 }
