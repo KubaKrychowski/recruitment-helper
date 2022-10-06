0d12098c4c0f7c3a948844eb197b3167b70aaf19
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-10-06 07:34:28 +0200

    added sign out button

--- Recruitment-helper/src/app/nav-bar/nav-bar.component.ts
+++ Recruitment-helper/src/app/nav-bar/nav-bar.component.ts
@@ -9,19 +9,28 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
-
   public get isUserAuthenticated(): boolean {
     return this.apiService.isUserAuthenticated;
   }
 
   public username: string | null = null;
 
-  constructor(private apiService: ApiService, private userService: UserService) {}
+  constructor(
+    private apiService: ApiService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
-    this.userService.currentUser.subscribe(user => {
-      this.username = user.username;
-      console.log(this.username);
-    })
+    this.userService.currentUser.subscribe((user) => {
+      if (user) {
+        this.username = user.username;
+      } else {
+        this.username = null;
+      }
+    });
+  }
+
+  public onSignOut(): void {
+    this.userService.signOut();
   }
 }
--- Recruitment-helper/src/app/services/user.service.ts
+++ Recruitment-helper/src/app/services/user.service.ts
@@ -1,13 +1,21 @@
+import { ApiService } from './api.service';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { SimplifiedUserDataModel } from '../shared/models/simplified-user-data';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  public currentUser: Subject<SimplifiedUserDataModel> =
-    new Subject<SimplifiedUserDataModel>();
-    
-  constructor() {}
+  public currentUser: Subject<SimplifiedUserDataModel | null> =
+    new Subject<SimplifiedUserDataModel | null>();
+
+  constructor(private apiService: ApiService,private router: Router) {}
+
+  public signOut(): void {
+    this.apiService.token = null;
+    this.currentUser.next(null);
+    this.router.navigate(['/home']);
+  }
 }
